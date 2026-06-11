3210633cf29f127dc847a96b50b98b8f331b2c9f
JonasGall <jonasicaro.17@gmail.com>
2026-06-12 01:19:49 +0800

    add home page

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Bata Cards!</h1>
+      <p>I-scan ang QR code sa iyong card para mag-play ng audio.</p>
+    </div>
+  );
+}
